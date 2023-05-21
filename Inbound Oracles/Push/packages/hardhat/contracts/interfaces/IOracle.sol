93fd6d2269fb35a1ce792d99794aa5b1ec1d20b5
Angry Coding <mailidanielifechukwu@gmail.com>
2023-05-21 15:16:49 +0100

    Changes

--- Inbound Oracles/Push/packages/hardhat/contracts/interfaces/IOracle.sol
+++ Inbound Oracles/Push/packages/hardhat/contracts/interfaces/IOracle.sol
@@ -2,11 +2,11 @@
 pragma solidity 0.8.17;
 
 interface IOracle {
-    function updateReporter(address reporterAddress, bool isReporter) external;
+  function updateReporter(address reporterAddress, bool isReporter) external;
 
-    function updateData(bytes32 key, uint payload) external;
+  function getReporter(address reporterAddress) external view returns (bool);
 
-    function getData(
-        bytes32 key
-    ) external view returns (bool results, uint date, uint payload);
+  function updateData(bytes32 key, uint payload) external;
+
+  function getData(bytes32 key) external view returns (bool results, uint date, uint payload);
 }

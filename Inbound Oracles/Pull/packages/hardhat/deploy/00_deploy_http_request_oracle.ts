f266b5a7def034a6e5811309698911c605944c74
Angry Coding <mailidanielifechukwu@gmail.com>
2023-05-25 23:36:08 +0100

    Deployment Script

--- Inbound Oracles/Pull/packages/hardhat/deploy/00_deploy_http_request_oracle.ts
+++ Inbound Oracles/Pull/packages/hardhat/deploy/00_deploy_http_request_oracle.ts
@@ -1,13 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
 
-/**
- * Deploys a contract named "YourContract" using the deployer account and
- * constructor arguments set to the deployer address
- *
- * @param hre HardhatRuntimeEnvironment object.
- */
-const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+const deployHttpRequestOracle: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   /*
     On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
 
@@ -20,23 +16,27 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   */
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
+  const [_, addr1] = await ethers.getSigners();
 
-  await deploy("YourContract", {
+  await deploy("HttpRequestOracle", {
     from: deployer,
-    // Contract constructor arguments
-    args: [deployer],
+    args: [],
     log: true,
-    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
-    // automatically mining the contract deployment transaction. There is no effect on live networks.
-    autoMine: true,
   });
 
-  // Get the deployed contract
-  // const yourContract = await hre.ethers.getContract("YourContract", deployer);
+  const httpRequestOracle = await hre.ethers.getContract("HttpRequestOracle", deployer);
+
+  // Add  a default reporter for the ui
+  // await priceOracle.updateReporter("0xCbdAa684708c13E7b85EBe3d3410DF02F8700808", true);
+
+  // other reporters
+  await httpRequestOracle.updateReporter(deployer, true);
+  await httpRequestOracle.updateReporter(addr1.address, true);
+  // await priceOracle.transferOwnership("0xCbdAa684708c13E7b85EBe3d3410DF02F8700808");
 };
 
-export default deployYourContract;
+export default deployHttpRequestOracle;
 
 // Tags are useful if you have multiple deploy files and only want to run one of them.
 // e.g. yarn deploy --tags YourContract
-deployYourContract.tags = ["YourContract"];
+deployHttpRequestOracle.tags = ["HttpRequestOracle"];
